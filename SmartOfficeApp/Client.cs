6af6ac71b6af378a58d6b618c6e0fcddb4472679
snaulX <alexandergunger666@gmail.com>
2022-05-07 19:21:42 +0600

    Change address for working

--- SmartOfficeApp/Client.cs
+++ SmartOfficeApp/Client.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SmartOfficeApp
 {
     public static class Client
     {
-        private const string HostIp = "https://127.0.0.1:5000/";
+        private const string HostIp = "http://127.0.0.1:5000/";
         private static HttpClient client;
 
         static Client()
